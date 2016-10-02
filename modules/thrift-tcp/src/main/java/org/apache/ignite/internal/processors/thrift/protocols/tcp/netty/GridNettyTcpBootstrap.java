fc36811da6650344263f20d3b48a8eb7fd3e356b
MauriceBetzel <info@betzel.net>
2016-10-02 20:32:33 +0200

    Start adding Thrift protocol

---
+++ modules/thrift-tcp/src/main/java/org/apache/ignite/internal/processors/thrift/protocols/tcp/netty/GridNettyTcpBootstrap.java
@@ -0,0 +1,7 @@
+package org.apache.ignite.internal.processors.thrift.protocols.tcp.netty;
+
+/**
+ * Created by mbetzel on 02.10.2016.
+ */
+public class GridNettyTcpBootstrap {
+}
--- modules/thrift-tcp/src/main/resources/idl/protocol.thrift
+++ modules/thrift-tcp/src/main/resources/idl/protocol.thrift
@@ -11,8 +11,64 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 namespace php org.apache.ignite.thrift.protocol
 namespace java org.apache.ignite.thrift.protocol
 
 const string PROTOCOL_VERSION = "1.8.0.0"
 const string APPLICATION_XTHRIFT = "application/x-thrift"
+
+enum GridClientPacketType {
+    MEMCACHE, THRIFT, IGNITE_HANDSHAKE, IGNITE_HANDSHAKE_RES, IGNITE
+
+enum GridCacheCommand {
+    DESTROY_CACHE,
+    GET_OR_CREATE_CACHE,
+    CACHE_CONTAINS_KEYS,
+    CACHE_CONTAINS_KEY,
+    CACHE_GET,
+    CACHE_GET_AND_PUT,
+    CACHE_GET_AND_REPLACE,
+    CACHE_GET_AND_PUT_IF_ABSENT,
+    CACHE_PUT_IF_ABSENT,
+    CACHE_GET_ALL,
+    CACHE_PUT,
+    CACHE_ADD,
+    CACHE_PUT_ALL,
+    CACHE_REMOVE,
+    CACHE_REMOVE_VALUE,
+    CACHE_REPLACE_VALUE,
+    CACHE_GET_AND_REMOVE,
+    CACHE_REMOVE_ALL,
+    CACHE_REPLACE,
+    CACHE_CAS,
+    CACHE_APPEND,
+    CACHE_PREPEND,
+    CACHE_METRICS,
+    CACHE_SIZE,
+    CACHE_METADATA
+}
+
+enum GridDataStructureCommand {
+    ATOMIC_INCREMENT, ATOMIC_DECREMENT
+}
+
+enum GridLogCommand {
+    LOG
+}
+
+enum GridQueryCommand {
+    EXECUTE_SQL_QUERY, EXECUTE_SQL_FIELDS_QUERY, EXECUTE_SCAN_QUERY, FETCH_SQL_QUERY, CLOSE_SQL_QUERY
+}
+
+enum GridTaskCommand {
+    EXE, RESULT, NOOP
+}
+
+enum GridTopologyCommand {
+    TOPOLOGY, NODE
+}
+
+enum GridVersionCommand {
+    VERSION, NAME
+}
